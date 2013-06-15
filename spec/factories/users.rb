a3e7eacc464949f21f2cc1adcb92b362665d98d3
Lin Reid <linreid@gmail.com>
2013-06-15 12:34:39 -0400

    user sign in test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@
 
 FactoryGirl.define do
   factory :user do
+    email = "foobar@example.com"
+    password = "12345678"
+    first_name = "Foo"
+    last_name = "Bar"
+    username = "FooBar"
+    role = "user"
   end
 end
---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe "A user signing in" do
+  
+  let(:registered_user) { FactoryGirl.build(:user) }
+
+  it "can sign in if they are a registered user" do
+    
+    visit root_path
+    click_on "Sign in"
+
+    fill_in "Email", with: registered_user.email
+    fill_in "Password", with: registered_user.password
+
+    expect(page).to have_content("Successfully signed in")
+    expect(page).to have_selector(:link_or_button "Sign out")
+    expect(current_user).to eql(registered_user)
+  end
+
+end
