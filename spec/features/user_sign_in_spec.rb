af9c7cf257186e0128c94beebaf9ce8a434e0ce7
Lin Reid <linreid@gmail.com>
2013-06-16 12:49:16 -0400

    User sign out link in navbar

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -2,19 +2,21 @@ require "spec_helper"
 
 describe "A user signing in" do
   
-  let(:registered_user) { FactoryGirl.build(:user) }
+  let!(:registered_user) { FactoryGirl.create(:user) }
 
   it "can sign in if they are a registered user" do
-    
+    valid_password = "12345678"
     visit root_path
-    click_on "Sign in"
+    click_link "Sign in"
 
     fill_in "Email", with: registered_user.email
-    fill_in "Password", with: registered_user.password
+    fill_in "Password", with: valid_password
+    within (".simple_form") do
+      click_button "Sign in"
+    end
 
-    expect(page).to have_content("Successfully signed in")
-    expect(page).to have_selector(link_or_button: "Sign out")
-    expect(current_user).to eql(registered_user)
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_link("Sign out")
   end
 
 end
