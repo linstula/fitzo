c6e43a0d4a7cd9b62742d3812de66f855919f24d
Lin Reid <linreid@gmail.com>
2013-06-18 20:13:44 -0400

    implement code review suggestions for refactoring profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,23 +1,11 @@
 class ProfilesController < ApplicationController
 
-  before_filter :user_is_trainer?, only: :show
-
-  def show  
-
-    if user_is_trainer? 
-      @user = User.find(params[:user_id])
-      @profile = @user.profile
-    else
-      redirect_to root_path, notice: "Resource does not exist"
-    end 
-    
-  end
-
-  private
-
-  def user_is_trainer?
+  def show
     @user = User.find(params[:user_id])
-    @user.role == "trainer" 
+
+    unless @user.role == "trainer"
+      redirect_to root_path, notice: "Resource does not exist"
+    end
   end
 
 end
