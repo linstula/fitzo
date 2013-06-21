61b1eb83b0b0ab6fac2dac1df7458e37751af5ec
Lin Reid <linreid@gmail.com>
2013-06-20 20:42:43 -0400

    Add blank service creation test and implementation

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -10,10 +10,12 @@ class ServicesController < ApplicationController
     @trainer_profile = TrainerProfile.find(params[:trainer_profile_id])
     @user = @trainer_profile.user
     @service = @trainer_profile.services.build(params[:service])
+    
     if @service.save
       redirect_to edit_user_trainer_profile_path(@user)
     else
-      render 'new', notice: "That was silly..."
+      flash.now[:notice] = "That was silly..."
+      render 'new'
     end
   end
 end
