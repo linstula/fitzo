19af6d587dc6cf1d9575b841f95aa4efd7a28ff9
Lin Reid <linreid@gmail.com>
2013-06-23 15:25:09 -0400

    user creates recommendation test and implementation

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -2,13 +2,15 @@ class RecommendationsController < ApplicationController
 
   def create
     @trainer_profile = TrainerProfile.find(params[:trainer_profile_id])
-    @recommendation = current_user.recommendations.build
+    @recommendation = @trainer_profile.recommendations.build(params[:recommendation])
     @recommendation.user = current_user
 
     if @recommendation.save
-      redirect_to user_trainer_profile_path(@trainer_profile.user), notice: "Recommendation added"
+      redirect_to user_trainer_profile_path(@trainer_profile.user),
+        notice: "Recommendation added"
     else
-      redirect_to user_trainer_profile_path(@trainer_profile.user), notice: "Recommendation was not added."
+      redirect_to user_trainer_profile_path(@trainer_profile.user), 
+        notice: "Recommendation was not added."
     end
   end
 end
--- spec/features/user_writes_a_recommendation_spec.rb
+++ spec/features/user_writes_a_recommendation_spec.rb
@@ -3,23 +3,21 @@ require "spec_helper"
 describe "user writes a recommendation for a trainer" do
   
   let(:recommendation_attr)   { FactoryGirl.attributes_for(:recommendation) }
-  let(:trainer_profile) { FactoryGirl.create(:trainer_profile) }
-  let(:trainer)   { trainer_profile.user }
+  let(:trainer)   { FactoryGirl.create(:trainer) } 
   let(:member)    { FactoryGirl.create(:member) }
 
   it "creates a recommendation with valid attributes" do
-    prev_count = trainer_profile.recommendations.count
+    profile = trainer.trainer_profile
+    prev_count = profile.recommendations.count
     sign_in(member)
     visit user_trainer_profile_path(trainer)
 
-    fill_in "Title", with: "This is a recommendaiton title."
-    fill_in "Content", with: "This is the content for a recommendation."
-    # fill_in_recommendation(recommendation_attr)
+    fill_in_recommendation(recommendation_attr)
     click_on "Create Recommendation"
 
-    expect(trainer_profile.recommendations.count).to eql(prev_count + 1)
+    expect(profile.recommendations.count).to eql(prev_count + 1)
     expect(current_path).to eql(user_trainer_profile_path(trainer))
-    expect(page).to have_content(recommendation[:title])
+    expect(page).to have_content(recommendation_attr[:title])
   end
 
   it "cannot be created if a user is not signed in"
@@ -29,8 +27,7 @@ describe "user writes a recommendation for a trainer" do
   it "cannot write a recommendation for their own profile"
 end
 
-# def fill_in_recommendation(recommendation)
-#   binding.pry
-#   fill_in "Title", with: recommendation[:title]
-#   fill_in "Content", with: recommendation[:content]
-# end
+def fill_in_recommendation(recommendation)
+  fill_in "Title", with: recommendation[:title]
+  fill_in "Content", with: recommendation[:content]
+end
