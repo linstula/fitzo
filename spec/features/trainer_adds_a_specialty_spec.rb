60647671f780034b9ce869c2dabf2a52d669498d
Lin Reid <linreid@gmail.com>
2013-06-24 17:13:15 -0400

    refactor specialties tests to reflect new routes

--- spec/features/trainer_adds_a_specialty_spec.rb
+++ spec/features/trainer_adds_a_specialty_spec.rb
@@ -15,42 +15,54 @@ feature "trainer adds a specialty", %{
   #   from DB for the specialty
 
   # let(:trainer_profile) { FactoryGirl.create(:trainer_profile) }
-  let(:trainer)         { FactoryGirl.create(:trainer) }
+  let(:trainer)         { FactoryGirl.build(:trainer) }
   let(:specialty_attr)  {  FactoryGirl.attributes_for(:specialty)}
   
   context "a signed-in trainer" do
-    before(:each) do
-      trainer.build_trainer_profile.save
-      @prev_count = trainer.trainer_profile.specialties.count
-      sign_in(trainer)
-      visit edit_trainer_profile_path(trainer.trainer_profile)
-      click_on "Add Specialty"
-    end
+  #   before(:each) do
+  #     trainer.build_trainer_profile.save
+  #     @prev_count = trainer.trainer_profile.specialties.count
+  #     sign_in(trainer)
+  #     visit edit_trainer_profile_path(trainer.trainer_profile)
+  #     click_on "Add Specialty"
+  #   end
 
     it "adds a specialty with valid attributes" do
+      sign_up(trainer)
+      new_trainer = User.last
+      profile = new_trainer.trainer_profile
+      prev_count = profile.specialties.count
+
+      visit new_trainer_profile_specialty_path(profile)
+
       fill_in_specialty(specialty_attr)
 
       click_on "Create Specialty"
       specialty = Specialty.last
 
-      expect(trainer_profile.specialties.count).to eql(@prev_count + 1)
-      expect(current_path).to eql(edit_trainer_profile_path(trainer.trainer_profile))
+      expect(profile.specialties.count).to eql(prev_count + 1)
+      expect(current_path).to eql(edit_trainer_profile_path(profile))
       expect(page).to have_content(specialty_attr[:title])
-      expect(page).to have_content("Service added.")
+      expect(page).to have_content("Specialty added.")
     end
 
     it "can see a newly created specialty on the profile page" do
+      sign_up(trainer)
+      new_trainer = User.last
+      profile = new_trainer.trainer_profile
+
+      visit new_trainer_profile_specialty_path(profile)
+
       fill_in_specialty(specialty_attr)
 
       click_on "Create Specialty"
+      specialty = Specialty.last
 
-      visit user_trainer_profile_path(trainer)
+      visit trainer_profile_path(profile)
 
-      expect(page).to have_content(specialty_attr[:title])
+      expect(page).to have_content(specialty.title)
     end
   end
-
-
 end
 
 def fill_in_specialty(specialty)
