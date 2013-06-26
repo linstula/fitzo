d34a415bd9252347940c3f60ae92344a7367e5bb
Lin Reid <linreid@gmail.com>
2013-06-26 17:17:14 -0400

    remove typo

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Fitzo::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
-  # Sample of named route:j
+  # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
