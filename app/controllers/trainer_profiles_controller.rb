328cf84f465fe019f4144094165f5cf983f2879c
Lin Reid <linreid@gmail.com>
2013-06-26 16:27:42 -0400

    remove extra file, refactor trainer sign in test

--- app/controllers/trainer_profiles_controller.rb
+++ app/controllers/trainer_profiles_controller.rb
@@ -14,12 +14,6 @@ class TrainerProfilesController < ApplicationController
   def edit
     @trainer_profile = TrainerProfile.find(params[:id])
     @trainer_specialties = @trainer_profile.trainer_specialties.build
-    # TODO refactor to use cancan authorization
-    # if @trainer_profile.owner?(current_user)
-    #   @trainer = @trainer_profile.user
-    # else
-    #   redirect_to root_path, notice: "Resource does not exist"
-    # end
     authorize! :manage, @trainer_profile
   end
 
