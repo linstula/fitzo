cb563cf654ab59620d4cd8dab819d87686cfc39b
Lin Reid <linreid@gmail.com>
2013-07-01 14:36:43 -0400

    need to filter by location and specialty

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
   def show
-    @locations = Location.all
-    @json = @locations.to_gmaps4rails
+    @trainer_profiles = TrainerProfile.search_for_profiles(params[:query])
+    @trainer_locations = []
+    @trainer_profiles.each do |profile|
+      profile.locations.each do |location|
+        @trainer_locations << location
+      end
+    end
+    @json = @trainer_locations.to_gmaps4rails
   end
 end
--- app/controllers/trainer_profiles_controller.rb
+++ app/controllers/trainer_profiles_controller.rb
@@ -4,6 +4,13 @@ class TrainerProfilesController < ApplicationController
 
   def index
     @trainer_profiles = TrainerProfile.search_for_profiles(params[:query])
+    @trainer_locations = []
+    @trainer_profiles.each do |profile|
+      profile.locations.each do |location|
+        @trainer_locations << location
+      end
+    end
+    @json = @trainer_locations.to_gmaps4rails  
   end
 
   def show
