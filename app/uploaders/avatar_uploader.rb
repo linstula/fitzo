842ea9bcc223a2e0cc9b552fb1f296bc1858b29a
Lin Reid <linreid@gmail.com>
2013-07-03 13:05:46 -0400

    set whitelist of image types as png, jpg, jpeg and gif

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -39,33 +39,16 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [50, 50]
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
   def default_url
     "/images/fallback/" + [version_name, "default_avatar.jpeg"].compact.join('_')
   end
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
