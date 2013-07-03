f05e686d0d2a90352ca2714a90e0619175717fd9
Lin Reid <linreid@gmail.com>
2013-07-03 13:28:23 -0400

    refactor upload feature test

--- spec/features/user_uploads_avatar_spec.rb
+++ spec/features/user_uploads_avatar_spec.rb
@@ -12,11 +12,11 @@ feature "User uploads avatar" do
     fill_in "user[current_password]", with: "12345678"
     attach_file('user_avatar', "#{Rails.root}/spec/images/test.jpg")    
     click_on "Update"
+    expect(member.avatar).to_not be nil
 
     visit edit_user_registration_path(member)
 
     expect("/uploads/user/avatar/" + member.id.to_s + "/test.jpg").to be_present
-    expect(page).to have_content(member.avatar_url(:large))
   end
 
 end
