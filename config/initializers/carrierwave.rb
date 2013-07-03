ad5ae2f7fb226556b97a28004510939c84d6c489
Lin Reid <linreid@gmail.com>
2013-07-02 20:34:16 -0400

    set carrierwave cache

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['S3_ACCESS_KEY_ID'],      # required
-      :aws_secret_access_key  => ENV['SECRET_KEY']             # required
+      :aws_secret_access_key  => ENV['SECRET_KEY'],            # required
+      :region                 => 'us-west-2'
     }
     config.fog_directory  = ENV['S3_BUCKET']                   # required
   end
