807090caebc889d9fc32f1586719bd21f3159b06
Lin Reid <linreid@gmail.com>
2013-07-03 13:37:20 -0400

    add test coverage for default avatar and file type

--- spec/features/user_uploads_avatar_spec.rb
+++ spec/features/user_uploads_avatar_spec.rb
@@ -19,4 +19,21 @@ feature "User uploads avatar" do
     expect("/uploads/user/avatar/" + member.id.to_s + "/test.jpg").to be_present
   end
 
+  it "cannot upload a file that is not a jpeg, jpg, png, or gif" do
+    sign_in(member)
+
+    visit edit_user_registration_path(member)
+
+    fill_in "user[current_password]", with: "12345678"
+    attach_file('user_avatar', "#{Rails.root}/spec/images/test.txt")    
+    click_on "Update"
+    expect(page).to have_content("You are not allowed to upload \"txt\" files, allowed types: jpg, jpeg, gif, png")
+  end
+
+  it "has a default avatar if no avatar is uploaded" do
+    sign_in(member)
+    expect(member.avatar).to_not be nil
+  end
+
+  
 end
