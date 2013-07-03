829c7a69d1296df6a4ba0346f583b752838455b3
Lin Reid <linreid@gmail.com>
2013-07-03 09:58:33 -0400

    remove region from carrierwave initilizer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,6 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['S3_ACCESS_KEY_ID'],      # required
       :aws_secret_access_key  => ENV['SECRET_KEY'],            # required
-      :region                 => 'us-west-2'
     }
     config.fog_directory  = ENV['S3_BUCKET']                   # required
   end
