7ed8dafa90a8a41414953c001a41309468272707
Lin Reid <linreid@gmail.com>
2013-07-05 14:04:44 -0400

    DRY out trainer profile unit tests

--- spec/models/trainer_profile_spec.rb
+++ spec/models/trainer_profile_spec.rb
@@ -12,12 +12,12 @@ describe TrainerProfile do
 
   it { should validate_presence_of(:user_id)}
 
+  let(:trainer) { FactoryGirl.create(:trainer) }
+  let(:profile) { trainer.trainer_profile }
+  let(:spec)    { FactoryGirl.create(:specialty, title: "First Specialty") }
+  let(:spec_2)  { FactoryGirl.create(:specialty, title: "Second Specialty")}
+
   it "should be able to add new specialties" do
-    spec    = Specialty.create(title: "First Specialty")
-    spec_2  = Specialty.create(title: "Second Specialty")
-    trainer = FactoryGirl.create(:trainer)
-    profile = trainer.build_trainer_profile
-    profile.save
     spec_ids = [spec.id, spec_2.id]
     prev_count = profile.trainer_specialties.count
 
@@ -26,11 +26,6 @@ describe TrainerProfile do
   end
 
   it "should be able to remove specialties" do
-    spec    = Specialty.create(title: "First Specialty")
-    spec_2  = Specialty.create(title: "Second Specialty")
-    trainer = FactoryGirl.create(:trainer)
-    profile = trainer.build_trainer_profile
-    profile.save
     spec_ids = [spec.id.to_s, spec_2.id.to_s]
     prev_count = profile.trainer_specialties.count
 
@@ -42,4 +37,8 @@ describe TrainerProfile do
     profile.remove_specialties(updated_spec_ids)
     expect(profile.trainer_specialties.count).to eql(current_count - 1)
   end
+
+  # it "can return it's owner's full name" do
+
+  # end
 end
