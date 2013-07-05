50183160d7e5342b78b0822ab6ce2d2b002be79d
Lin Reid <linreid@gmail.com>
2013-07-05 17:14:18 -0400

    add phone_number, website, and about to trainer_profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130702175820) do
+ActiveRecord::Schema.define(:version => 20130705204924) do
 
   create_table "locations", :force => true do |t|
     t.string   "street_address"
@@ -62,9 +62,12 @@ ActiveRecord::Schema.define(:version => 20130702175820) do
   end
 
   create_table "trainer_profiles", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "user_id",    :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.integer  "user_id",                      :null => false
+    t.string   "phone_number", :default => ""
+    t.string   "website",      :default => ""
+    t.text     "about",        :default => ""
   end
 
   create_table "trainer_specialties", :force => true do |t|
